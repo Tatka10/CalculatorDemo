da3366fe294bc656380a898de294a606c63068be
Tatka 10 <ccards@bk.ru>
2024-11-14 20:28:03 +0300

    ДОбавили калькулятор

---
+++ src/main/java/com/example/CalculatorDemo/CalcController.java
@@ -0,0 +1,9 @@
+package com.example.CalculatorDemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CalcController {
+
+}
